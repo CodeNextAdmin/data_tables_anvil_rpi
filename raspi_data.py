591ba2e368e8fdb51e271677e8134c895cf4b214
CodeNextPaco <nietof@google.com>
2021-03-31 12:53:01 -0700

    first commit

---
+++ raspi_data.py
@@ -0,0 +1,49 @@
+import anvil.server
+from anvil.tables import app_tables
+from sense_emu import SenseHat
+from time import sleep
+from datetime import datetime
+
+sense = SenseHat()
+ 
+ 
+anvil.server.connect("LCVW7GRCOJLKWOJPQ7QWJK6I-HBPGXTB5NGHBE7EK")
+
+
+@anvil.server.callable
+def get_env_data():
+    
+    humidity = sense.get_humidity()
+    temperature = sense.get_temperature()
+    pressure = sense.get_pressure()
+    
+    data = {
+        "h":humidity,
+        "t": temperature,
+        "p": pressure
+        
+        }
+    return data
+
+@anvil.server.callable
+def save_data():
+    
+    data = get_env_data()
+    t = data["t"]
+    h = data["h"]
+    p = data["p"]
+    dt = datetime.now()
+    
+    new_row = app_tables.env_data.add_row(datetime=dt,
+                                          temperature=t,
+                                          humidity=h,
+                                          pressure=p)
+    
+    print(new_row)
+    
+    
+    
+
+while True:
+    save_data()
+    sleep(10)
